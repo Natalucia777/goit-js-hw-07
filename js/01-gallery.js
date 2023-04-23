9b3857dfb34e36f9fd2f53b1f7b526329ef281fe
Natalya Demidenko <natalucia777@ukr.net>
2023-04-23 12:42:59 +0300

    hw-7-5

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 const galleryContent = document.querySelector('.gallery');
 galleryContent.innerHTML = markup(galleryItems);
 galleryContent.addEventListener('click', onGetImage);
+
 function markup(items) {
-  return items
-    .map(({ preview, original, description }) => {
+  return items.map(({ preview, original, description }) => {
       return `<li class="gallery__item">
             <a class="gallery__link" href="${original}">
                 <img class="gallery__img" src="${preview}" 
-                data-source="${original}" alt="${description}" />
-            </a>
-            </li>`; }).join('');}
-function onActiveButton(element) {
-    if (element.code === "Escape") {
-      instance.close();}
-}
- function onGetImage(element) {
-  element.preventDefault();
-  const { target } = element;
+                data-source="${original}" alt="${description}" /></a></li>`;}).join('');}
+
+function onGetImage(e) {
+  e.preventDefault();
+  const { target } = e;
   if (!target.classList.contains('gallery_img')) {
     return; }
-  const content = basicLightbox.create(`<img src="${target.dataset.cource}"/>`,
+  const instance = basicLightbox.create(`<img src="${target.dataset.cource}"/>`,
     { onShow: () => window.addEventListener('keydown', onActiveButton),
       onClose: () => window.removeEventListener('keydown', onActiveButton), });
-  content.show(); 
+  instance.show(); 
+
+  function onActiveButton(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
 }
-
-
-
-// console.log(galleryItems);  
