5c1826ccb6190dd19e0557d05b0fbf167f9aeab1
Natalya Demidenko <natalucia777@ukr.net>
2023-04-23 13:18:17 +0300

    hw-7-1-6

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,28 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-const galleryContent = document.querySelector('.gallery');
-galleryContent.innerHTML = markup(galleryItems);
-galleryContent.addEventListener('click', onGetImage);
 
+const galleryContent = document.querySelector('.gallery');
 function markup(items) {
   return items.map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img class="gallery__img" src="${preview}" 
-                data-source="${original}" alt="${description}" /></a></li>`;}).join('');}
+      return `<li class="gallery__item"><a class="gallery__link" href="${original}">
+              <img class="gallery__img" src="${preview}" data-source="${original}" 
+              alt="${description}" /></a></li>`; }).join('');}
+galleryContent.innerHTML = markup(galleryItems);
 
-function onGetImage(e) {
-  e.preventDefault();
+galleryContent.addEventListener('click', onGetImage);
+function onGetImage(e) { e.preventDefault();
   const { target } = e;
-  if (!target.classList.contains('gallery_img')) {
+  if (!target.classList.contains('gallery__img')) {
     return; }
-  const instance = basicLightbox.create(`<img src="${target.dataset.cource}"/>`,
-    { onShow: () => window.addEventListener('keydown', onActiveButton),
-      onClose: () => window.removeEventListener('keydown', onActiveButton), });
-  instance.show(); 
-
-  function onActiveButton(e) {
-    if (e.code === "Escape") {
-      instance.close();
-    }
+  const instance = basicLightbox.create(`<img src="${target.dataset.source}"/>`, {
+    onShow: () => window.addEventListener('keydown', onEscButton),
+    onClose: () => window.removeEventListener('keydown', onEscButton), });
+  instance.show();
+  function onEscButton(e) {
+    if (e.code === 'Escape') { instance.close(); }
   }
 }
